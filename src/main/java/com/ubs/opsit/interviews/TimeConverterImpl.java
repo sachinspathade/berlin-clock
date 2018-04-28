46c26c6ea239f19005e5794ae68ed765015f9619
HP <HP@HP-HP>
2018-04-28 18:50:23 +0530

    Implementation class

---
+++ src/main/java/com/ubs/opsit/interviews/TimeConverterImpl.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.ubs.opsit.interviews;
+
+/**
+ * @author HP
+ *
+ */
+public class TimeConverterImpl implements TimeConverter {
+
+	/* (non-Javadoc)
+	 * @see com.ubs.opsit.interviews.TimeConverter#convertTime(java.lang.String)
+	 */
+	@Override
+	public String convertTime(String aTime) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
